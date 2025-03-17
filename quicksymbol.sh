c3ede74c5f5ffde7fcf04593162fe293c85dbaa9
Blair Wang <emotion-tells0j@icloud.com>
2025-03-17 17:36:54 +0000

    [feat] More symbols

--- quicksymbol.sh
+++ quicksymbol.sh
@@ -7,13 +7,25 @@ case "$1" in
   mdash)
     echo "—"
     ;;
-  lstroke)
+  l_stroke)
     echo "ł"
     ;;
-  Lstroke)
+  L_stroke)
     echo "Ł"
     ;;
+  o_umlaut)
+    echo "ö"
+    ;;
+  O_umlaut)
+    echo "Ö"
+    ;;
+  o_slash)
+    echo "ø"
+    ;;
+  O_slash)
+    echo "Ø"
+    ;;
   *)
-    echo "Please specify the desired symbol, e.g.: euro / mdash"
+    echo "Please specify the desired symbol, e.g.: euro, mdash, l_stroke, L_stroke, o_umlaut, O_umlaut, o_slash, O_slash"
     ;;
 esac
